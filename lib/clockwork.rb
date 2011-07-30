ff51d303cd3d27700f56b37a1df930032f215fd3
Lukáš Konarovský <daeltar@gmail.com>
2011-07-30 17:18:20 +0200

    Fix possible double-extending Numeric.

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -103,18 +103,21 @@ module Clockwork
 		@@events = []
 		@@handler = nil
 	end
+	
 end
 
-class Numeric
-	def seconds; self; end
-	alias :second :seconds
+unless 1.respond_to?(:seconds) 
+  class Numeric
+  	def seconds; self; end
+  	alias :second :seconds
 
-	def minutes; self * 60; end
-	alias :minute :minutes
+  	def minutes; self * 60; end
+  	alias :minute :minutes
 
-	def hours; self * 3600; end
-	alias :hour :hours
+  	def hours; self * 3600; end
+  	alias :hour :hours
 
-	def days; self * 86400; end
-	alias :day :days
+  	def days; self * 86400; end
+  	alias :day :days
+  end
 end
