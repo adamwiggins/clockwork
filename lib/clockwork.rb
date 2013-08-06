b1f171c35f951e3845ae92ef6152871c74639646
tomykaira <tomykaira@gmail.com>
2013-08-06 22:19:08 +0900

    Move functions to Manager from Clockwork
    Clockwork module has only the features useful.
    This patch reduces the name conflict problem with other gems adding methods to global namespace.

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -6,90 +6,29 @@ require 'clockwork/event'
 require 'clockwork/manager'
 
 module Clockwork
-
-  @@events = []
-
-  def thread_available?
-    Thread.list.count < config[:max_threads]
-  end
-
-  def configure
-    yield(config)
-  end
-
-  def config
-    @@configuration
-  end
-
   extend self
 
-  def default_configuration
-    { :sleep_timeout => 1, :logger => Logger.new(STDOUT), :thread => false, :max_threads => 10 }
-  end
+  @@manager = Manager.new
 
-  @@configuration = default_configuration
+  def configure(&block)
+    @@manager.configure(&block)
+  end
 
   def handler(&block)
-    @@handler = block
-  end
-
-  class NoHandlerDefined < RuntimeError; end
-
-  def get_handler
-    raise NoHandlerDefined unless (defined?(@@handler) and @@handler)
-    @@handler
+    @@manager.handler(&block)
   end
 
   def every(period, job, options={}, &block)
-    if options[:at].respond_to?(:each)
-      each_options = options.clone
-      options[:at].each do |at|
-        each_options[:at] = at
-        register(period, job, block, each_options)
-      end
-    else
-      register(period, job, block, options)
-    end
+    @@manager.every(period, job, options, &block)
   end
 
   def run
-    log "Starting clock for #{@@events.size} events: [ " + @@events.map { |e| e.to_s }.join(' ') + " ]"
-    loop do
-      tick
-      sleep(config[:sleep_timeout])
-    end
-  end
-
-  def log(msg)
-    config[:logger].info(msg)
-  end
-
-  def tick(t=Time.now)
-    to_run = @@events.select do |event|
-      event.time?(t)
-    end
-
-    to_run.each do |event|
-      log "Triggering '#{event}'"
-      event.run(t)
-    end
-
-    to_run
+    @@manager.run
   end
 
   def clear!
-    @@events = []
-    @@handler = nil
-    @@configuration = Clockwork.default_configuration
+    @@manager = Manager.new
   end
-
-  private
-  def register(period, job, block, options)
-    event = Event.new(period, job, block || get_handler, options)
-    @@events << event
-    event
-  end
-
 end
 
 unless 1.respond_to?(:seconds)
