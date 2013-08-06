2612b284480b64927a20fdb4bbfdcf33b69338c6
tomykaira <tomykaira@gmail.com>
2013-08-06 11:47:10 +0900

    Move inner classes to individual files

---
+++ lib/clockwork/event.rb
@@ -0,0 +1,78 @@
+module Clockwork
+  class Event
+    attr_accessor :job, :last
+
+    def initialize(period, job, block, options={})
+      @period = period
+      @job = job
+      @at = At.parse(options[:at])
+      @last = nil
+      @block = block
+      if options[:if]
+        if options[:if].respond_to?(:call)
+          @if = options[:if]
+        else
+          raise ArgumentError.new(':if expects a callable object, but #{options[:if]} does not respond to call')
+        end
+      end
+
+      @thread = !!(options.has_key?(:thread) ? options[:thread] : Clockwork.config[:thread])
+
+      @timezone = options[:tz] || Clockwork.config[:tz]
+    end
+
+    def to_s
+      @job
+    end
+
+    def convert_timezone(t)
+      @timezone ? t.in_time_zone(@timezone) : t
+    end
+
+    def time?(t)
+      t = convert_timezone(t)
+      elapsed_ready = (@last.nil? or (t - @last).to_i >= @period)
+      elapsed_ready and (@at.nil? or @at.ready?(t)) and (@if.nil? or @if.call(t))
+    end
+
+    def thread?
+      @thread
+    end
+
+    def run(t)
+      t = convert_timezone(t)
+      @last = t
+
+      if thread?
+        if Clockwork.thread_available?
+          Thread.new { execute }
+        else
+          log_error "Threads exhausted; skipping #{self}"
+        end
+      else
+        execute
+      end
+    end
+
+    def execute
+      @block.call(@job)
+    rescue => e
+      log_error e
+    end
+
+    def log_error(e)
+      Clockwork.config[:logger].error(e)
+    end
+
+    def exception_message(e)
+      msg = [ "Exception #{e.class} -> #{e.message}" ]
+
+      base = File.expand_path(Dir.pwd) + '/'
+      e.backtrace.each do |t|
+        msg << "   #{File.expand_path(t).gsub(/#{base}/, '')}"
+      end
+
+      msg.join("\n")
+    end
+  end
+end
