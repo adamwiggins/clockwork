06a02712abd086c72ab634fb8bd3bbc2202ed453
Takatoshi Maeda <me@tmd.tw>
2013-10-28 18:35:52 +0900

    log include time check testcase removed.

--- test/clockwork_test.rb
+++ test/clockwork_test.rb
@@ -75,12 +75,11 @@ class ClockworkTest < Test::Unit::TestCase
 
   test 'should pass time to handler block' do
     Clockwork.every(1.second, 'myjob') {|job, time| puts "execute_at:#{time}" }
-    string_io = set_string_io_logger
+    set_string_io_logger
     now = Time.now
     out, err = capture_io {
       Clockwork.class_variable_get(:@@manager).tick(now)
     }
-    assert string_io.string.include?("at #{now}")
     assert out.include?("execute_at:#{now}")
   end
 end
