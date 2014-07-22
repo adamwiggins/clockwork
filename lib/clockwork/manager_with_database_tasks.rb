df583bdb4b72711aef5d054fcc24acd3eeeb0e6c
Rob Isenberg <robisenberg@me.com>
2014-07-22 08:22:20 +0100

    Update terminology from task to db_event

--- lib/clockwork/manager_with_database_tasks.rb
+++ lib/clockwork/manager_with_database_tasks.rb
@@ -10,8 +10,10 @@ module Clockwork
   end
 
   module Methods
-    def sync_database_tasks(options={}, &block)
-      Clockwork.manager.sync_database_tasks(options, &block)
+    alias :sync_database_tasks, :sync_database_events
+
+    def sync_database_events(options={}, &block)
+      Clockwork.manager.sync_database_events(options, &block)
     end
   end
 
@@ -26,97 +28,97 @@ module Clockwork
       @events = {}
     end
 
-    # Ensure clockwork events reflect database tasks
-    # Adds any new tasks, modifies updated ones, and delets removed ones
+    # Ensure clockwork events reflect database events
+    # Adds any new events, modifies updated ones, and delets removed ones
     def sync
       model_ids_that_exist = []
 
-      @model.all.each do |db_task|
-        model_ids_that_exist << db_task.id
+      @model.all.each do |db_event|
+        model_ids_that_exist << db_event.id
         
-        if !event_exists_for_task(db_task) || task_has_changed(db_task)
-          recreate_event_for_database_task(db_task)
+        if !clockwork_event_exists_for_db_event(db_event) || db_event_has_changed(db_event)
+          recreate_clockwork_event_for_db_event(db_event)
         end
       end
 
-      remove_deleted_database_tasks(model_ids_that_exist)
+      remove_deleted_db_events(model_ids_that_exist)
     end
 
     def clockwork_events
       @events.values.flatten
     end
 
-    # store events by task_id in array (array is needed as there is 1 event per At)
-    def add_event(e, task_id)
-      @events[task_id] ||= []
-      @events[task_id] << e
+    # store events by database event id in array (array is needed as there is 1 event per At)
+    def add_event(e, db_event_id)
+      @events[db_event_id] ||= []
+      @events[db_event_id] << e
     end
 
     protected
 
-      def recreate_event_for_database_task(db_task)
-        @events[db_task.id] = nil
+      def recreate_clockwork_event_for_db_event(db_event)
+        @events[db_event.id] = nil
 
         options = { 
           :from_database => true, 
-          :db_task_id =>  db_task.id,
+          :db_event_id =>  db_event.id,
           :performer => self,
-          :at => array_of_ats_for(db_task, :nil_if_empty => true)
+          :at => array_of_ats_for(db_event, :nil_if_empty => true)
         }
 
-        if db_task.respond_to?(:tz)
-          options[:tz] = db_task.tz
+        if db_event.respond_to?(:tz)
+          options[:tz] = db_event.tz
         end
 
-        @manager.every db_task.frequency, db_task.name, options, &@block
+        @manager.every db_event.frequency, db_event.name, options, &@block
       end
 
-      def event_exists_for_task(db_task)
-        @events[db_task.id]
+      def clockwork_event_exists_for_db_event(db_event)
+        @events[db_event.id]
       end
 
-      def remove_deleted_database_tasks(model_ids_that_exist)
-        @events.reject!{|db_task_id, _| !model_ids_that_exist.include?(db_task_id) }
+      def remove_deleted_db_events(model_ids_that_exist)
+        @events.reject!{|db_event_id, _| !model_ids_that_exist.include?(db_event_id) }
       end
 
-      def task_has_changed(task)
-        events = @events[task.id]
-        event = @events[task.id].first # all events will have same frequency/name, just different ats
-        ats_for_task = array_of_ats_for(task)
-        ats_from_event = array_of_ats_from_event(task.id)
+      def db_event_has_changed(db_event)
+        events = @events[db_event.id]
+        clockwork_event = @events[db_event.id].first # all events will have same frequency/name, just different ats
+        ats_for_db_event = array_of_ats_for(db_event)
+        ats_from_clockwork_event = array_of_ats_from_clockwork_event(db_event.id)
 
-        name_has_changed = task.name != event.job
-        frequency_has_changed = task.frequency != event.instance_variable_get(:@period)
+        name_has_changed = db_event.name != clockwork_event.job
+        frequency_has_changed = db_event.frequency != clockwork_event.instance_variable_get(:@period)
 
-        at_has_changed = ats_for_task.length != ats_from_event.length
-        at_has_changed ||= ats_for_task.inject(false) do |memo, at|
-          memo ||= !ats_from_event.include?(At.parse(at))
+        at_has_changed = ats_for_db_event.length != ats_from_clockwork_event.length
+        at_has_changed ||= ats_for_db_event.inject(false) do |memo, at|
+          memo ||= !ats_from_clockwork_event.include?(At.parse(at))
         end
 
         name_has_changed || frequency_has_changed || at_has_changed
       end
 
-      def array_of_ats_from_event(task_id)
-        @events[task_id].collect{|clockwork_event| clockwork_event.instance_variable_get(:@at) }.compact
+      def array_of_ats_from_clockwork_event(db_event_id)
+        @events[db_event_id].collect{|clockwork_event| clockwork_event.instance_variable_get(:@at) }.compact
       end
 
-      def array_of_ats_for(task, opts={})
-        if task.at.to_s.empty?
+      def array_of_ats_for(db_event, opts={})
+        if db_event.at.to_s.empty?
           opts[:nil_if_empty] ? nil : []
         else
-          task.at.split(',').map(&:strip)
+          db_event.at.split(',').map(&:strip)
         end
       end
   end
 
-  class ManagerWithDatabaseTasks < Manager
+  class ManagerWithDatabaseEvents < Manager
 
     def initialize
       super
       @database_event_sync_performers = []
     end
 
-    def sync_database_tasks(options={}, &block)
+    def sync_database_db_events(options={}, &block)
       [:model, :every].each do |option|
         raise ArgumentError.new("requires :#{option} option") unless options.include?(option)
       end
@@ -126,7 +128,7 @@ module Clockwork
       @database_event_sync_performers << sync_performer
 
       # create event that syncs clockwork events with database events
-      every options[:every], "sync_database_tasks_for_model_#{options[:model]}" do
+      every options[:every], "sync_database_events_for_model_#{options[:model]}" do
         sync_performer.sync
       end
     end
@@ -144,7 +146,7 @@ module Clockwork
     def register(period, job, block, options)
       Event.new(self, period, job, block || handler, options).tap do |e|
         if options[:from_database]
-          options[:performer].add_event(e, options[:db_task_id])
+          options[:performer].add_event(e, options[:db_event_id])
         else
           @events << e
         end
