7189777219cc1f94b4dfb6bbba61d4dbf54ae139
tomykaira <tomykaira@gmail.com>
2014-10-01 19:23:26 +0900

    Remove custom numeric overrides
    Use those from ActiveSupport.
    This fixes #122.

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -53,22 +53,3 @@ module Clockwork
 
   extend Methods
 end
-
-unless 1.respond_to?(:seconds)
-  class Numeric
-    def seconds; self; end
-    alias :second :seconds
-
-    def minutes; self * 60; end
-    alias :minute :minutes
-
-    def hours; self * 3600; end
-    alias :hour :hours
-
-    def days; self * 86400; end
-    alias :day :days
-
-    def weeks; self * 604800; end
-    alias :week :weeks
-  end
-end
